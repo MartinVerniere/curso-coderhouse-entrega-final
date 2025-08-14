b9fd8da52d3d8436fbc00ec9a42e25500d97ea50
MartinVerniere <martincho7verniere@yahoo.com>
2025-08-14 10:30:52 -0300

    Se modificaron html, y ahora delete se hace con una funcion en vez de una view

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('vehicles/<int:pk>', views.VehicleDetailView.as_view(), name='vehicle_details'),
     path('vehicles/create', views.VehicleCreateView.as_view(), name='vehicle_create'),
     path('vehicles/update/<int:pk>', views.VehicleUpdateView.as_view(), name='vehicle_update'),
-    path('vehicles/delete/<int:pk>', views.VehicleDeleteView.as_view(), name='vehicle_delete'),
+    #path('vehicles/delete/<int:pk>', views.VehicleDeleteView.as_view(), name='vehicle_delete'),
+    path('vehicles/delete/<int:pk>', views.delete_vehicle, name='vehicle_delete'),
     path('aboutme', views.aboutMe, name='about_me'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.db.models import Q
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from .forms import VehicleForm
 from .models import Vehicle
 
@@ -30,7 +31,7 @@ class VehicleCreateView(CreateView):
 class VehicleDetailView(DetailView):
     model = Vehicle
 
-class VehicleUpdateView(UpdateView):
+class VehicleUpdateView(LoginRequiredMixin, UpdateView):
     model = Vehicle
     form_class = VehicleForm
     success_url = reverse_lazy('vehicle_list')
@@ -38,9 +39,17 @@ class VehicleUpdateView(UpdateView):
     def form_valid(self, form):
         return super().form_valid(form)
     
-class VehicleDeleteView(DeleteView):
+class VehicleDeleteView(LoginRequiredMixin, DeleteView):
     model = Vehicle
     success_url = reverse_lazy('vehicle_list')
+
+@login_required
+def delete_vehicle(request, pk):
+    vehicle = get_object_or_404(Vehicle, pk=pk)
+    if request.method == 'POST':
+        vehicle.delete()
+        return redirect(reverse_lazy('vehicle_list'))
+    return render(request, 'myapp/vehicle_confirm_delete.html', {'vehicle': vehicle})
 
 def home(request):
     return render(request, 'myapp/home.html')
